8eb7094f728d1bc45214f44924bbcd79842f304d
Gregory Petit <petitg1987@gmail.com>
2018-10-27 13:00:53 +0200

    H2: use server mode to allow multiple connection

--- src/main/java/com/urchin/release/mgt/UrchinReleaseMgtApplication.java
+++ src/main/java/com/urchin/release/mgt/UrchinReleaseMgtApplication.java
@@ -1,12 +1,17 @@
 package com.urchin.release.mgt;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class UrchinReleaseMgtApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		Server.createTcpServer().start();
+
 		SpringApplication.run(UrchinReleaseMgtApplication.class, args);
 	}
 
