524ca9f5958ddc1b7e53fc4129d4d5c2096a611f
Gregory Petit <petitg1987@gmail.com>
2018-10-30 20:41:14 +0100

    Binary: fix extension for Windows

--- src/main/java/com/urchin/release/mgt/model/BinaryType.java
+++ src/main/java/com/urchin/release/mgt/model/BinaryType.java
@@ -3,7 +3,7 @@ package com.urchin.release.mgt.model;
 public enum BinaryType {
     LINUX_TAR("tar.bz2"),
     LINUX_DEB("deb"),
-    WINDOWS("exe");
+    WINDOWS("msi");
 
     private String extension;
 
