33018bf6242f6a7be81b209eee89c48de2261cd7
Gregory Petit <petitg1987@gmail.com>
2018-12-16 10:50:51 +0100

    Deploy script on AWS

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #Pre-requisites:
+# Define AWS credential and region: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 # Setup EB CLI: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-install.html
 # Configure EB CLI: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-configuration.html
 
 if [[ -z $1 ]]; then
-    echo "Application name is required (example: GreenCityReleaseMgt)"
+    echo "Application name is required (example: GreenCity)"
     exit 1
 fi
 
@@ -14,18 +15,25 @@ if ! [[ "$2" =~ ^(delete|init|deploy)$ ]]; then
     exit 1
 fi
 
+appName="$1ReleaseMgt"
 appVersion=`cat pom.xml | grep -Eo "[0-9]\.[0-9]\.[0-9]</version>" | head -1 | cut -d'<' -f1`
-echo "Start '$2' action on application version $appVersion"
+cnamePrefix=`echo "$appName" | tr '[:upper:]' '[:lower:]'`
+echo "Execute '$2' action on application '$appName/$appVersion' with CNAME prefix '$cnamePrefix'"
 
 cd "$(dirname "$0")"
 mvn clean install
 
 if [[ "$2" == "init" ]]; then
-    eb init $1
-    eb create prod-env
+    rm -rf ./.elasticbeanstalk/config.yml
+    mkdir -p ./.elasticbeanstalk
+    echo "deploy:" >> ./.elasticbeanstalk/config.yml
+    echo "  artifact: target/urchin-release-mgt.zip" >> ./.elasticbeanstalk/config.yml
+
+    eb init ${appName}
+    eb create prod-env --cname ${cnamePrefix}
 elif [[ "$2" == "deploy" ]]; then
     eb deploy prod-env
 elif [[ "$2" == "delete" ]]; then
-    eb terminate prod-env
-    echo "Please, delete manually AWS S3 bucket linked to elastic beanstalk..."
+    eb terminate prod-env --all
+    echo "AWS S3 bucket linked to elastic beanstalk can be deleted"
 fi
