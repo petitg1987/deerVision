fc58f1f0c63324a4281b77905fb131239a0ccdaa
greg <petitg1987@gmail.com>
2019-05-24 17:21:48 +0200

    Terraform update

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -1,3 +1,13 @@
+##########################################################################################
+# PRE-REQUISITE
+##########################################################################################
+# 1) Create IAM User with programmatic access & "AdministratorAccess" IAM policy
+# 2) Add keys in ~/.aws/credentials under "[releasemgt]" profile
+# 3) Use: terraform init && terraform apply -auto-approve
+
+##########################################################################################
+# CREDENTIAL
+##########################################################################################
 provider "aws" {
   region = "eu-west-3"
   shared_credentials_file = "~/.aws/credentials"
@@ -5,7 +15,7 @@ provider "aws" {
 }
 
 ##########################################################################################
-# NETWORK
+# NETWORK & ACCESS
 ##########################################################################################
 resource "aws_vpc" "release_mgt_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -39,12 +49,12 @@ resource "aws_subnet" "release_mgt_subnet" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.release_mgt_subnet.id}"
+  subnet_id = "${aws_subnet.release_mgt_subnet.id}"
   route_table_id = "${aws_route_table.release_mgt_route.id}"
 }
 
 resource "aws_security_group" "release_mgt_sg" {
-  name        = "releaseMgtSG"
+  name = "releaseMgtSG"
   description = "Release Mgt Security Group"
   vpc_id = "${aws_vpc.release_mgt_vpc.id}"
   ingress {
@@ -61,14 +71,14 @@ resource "aws_security_group" "release_mgt_sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Ping"
   }
-  ingress { #TODO remove ?
+  ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "HTTP"
   }
-  ingress {
+  ingress { #TODO remove (use SSL until ELB) ?
     from_port = 443
     to_port = 443
     protocol = "tcp"
@@ -81,28 +91,78 @@ resource "aws_security_group" "release_mgt_sg" {
 }
 
 ##########################################################################################
+# LOAD BALANCER
+##########################################################################################
+resource "aws_lb_target_group" "release_mgt_elb_target_group" {
+  name = "tf-example-lb-tg"
+  port= 443
+  protocol = "HTTPS"
+  vpc_id = "${aws_vpc.release_mgt_vpc.id}"
+  health_check {
+    enabled = false #TODO active ELB health check
+  }
+}
+
+resource "aws_lb" "release_mgt_elb" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.release_mgt_sg.id}"]
+  subnets            = ["${aws_subnet.release_mgt_subnet.id}"]
+
+  enable_deletion_protection = true
+
+#TODO  access_logs {
+#    bucket  = "${aws_s3_bucket.lb_logs.bucket}"
+#    prefix  = "test-lb"
+#    enabled = true
+#  }
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = "${aws_lb_target_group.release_mgt_elb_target_group.arn}"
+  target_id        = "${aws_instance.test.id}"
+  port             = 80
+}
+
+##########################################################################################
 # INSTANCES
 ##########################################################################################
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
-    name   = "name"
+    name = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
   filter {
-    name   = "virtualization-type"
+    name = "virtualization-type"
     values = ["hvm"]
   }
   owners = ["099720109477"]
 }
 
-resource "aws_instance" "release_mgt_instance" {
-  ami = "${data.aws_ami.ubuntu.id}"
+resource "aws_launch_template" "release_mgt_launch_template" {
+  name_prefix = "releaseMgtInstance"
+  image_id = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.release_mgt_subnet.id}"
-  security_groups = ["${aws_security_group.release_mgt_sg.id}"]
   key_name = "releasemgt"
-  tags = {
-    Name = "releaseMgtInstance"
+  vpc_security_group_ids = ["${aws_security_group.release_mgt_sg.id}"]
+}
+
+resource "aws_autoscaling_group" "release_mgt_asg" {
+  desired_capacity = 1
+  max_size = 1
+  min_size = 1
+  vpc_zone_identifier = ["${aws_subnet.release_mgt_subnet.id}"]
+  health_check_grace_period = 300
+  health_check_type = "ELB"
+  tag {
+    key = "Name"
+    value = "releaseMgtInstance"
+    propagate_at_launch = true
+  }
+  launch_template {
+    id = "${aws_launch_template.release_mgt_launch_template.id}"
+    version = "$Latest"
   }
 }
