dafb4b17db8b8fe9b2bf0c33351851af5e498420
greg <petitg1987@gmail.com>
2019-05-24 19:24:54 +0200

    Terraform update (elb and route 53)

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -42,14 +42,27 @@ resource "aws_route_table" "release_mgt_route" {
   }
 }
 
-resource "aws_subnet" "release_mgt_subnet" {
+resource "aws_subnet" "release_mgt_subnet_a" {
   vpc_id = "${aws_vpc.release_mgt_vpc.id}"
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = true
+  availability_zone = "eu-west-3a"
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id = "${aws_subnet.release_mgt_subnet.id}"
+resource "aws_subnet" "release_mgt_subnet_b" {
+  vpc_id = "${aws_vpc.release_mgt_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-3b"
+}
+
+resource "aws_route_table_association" "release_mgt_route_a" {
+  subnet_id = "${aws_subnet.release_mgt_subnet_a.id}"
+  route_table_id = "${aws_route_table.release_mgt_route.id}"
+}
+
+resource "aws_route_table_association" "release_mgt_route_b" {
+  subnet_id = "${aws_subnet.release_mgt_subnet_b.id}"
   route_table_id = "${aws_route_table.release_mgt_route.id}"
 }
 
@@ -91,41 +104,6 @@ resource "aws_security_group" "release_mgt_sg" {
 }
 
 ##########################################################################################
-# LOAD BALANCER
-##########################################################################################
-resource "aws_lb_target_group" "release_mgt_elb_target_group" {
-  name = "tf-example-lb-tg"
-  port= 443
-  protocol = "HTTPS"
-  vpc_id = "${aws_vpc.release_mgt_vpc.id}"
-  health_check {
-    enabled = false #TODO active ELB health check
-  }
-}
-
-resource "aws_lb" "release_mgt_elb" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.release_mgt_sg.id}"]
-  subnets            = ["${aws_subnet.release_mgt_subnet.id}"]
-
-  enable_deletion_protection = true
-
-#TODO  access_logs {
-#    bucket  = "${aws_s3_bucket.lb_logs.bucket}"
-#    prefix  = "test-lb"
-#    enabled = true
-#  }
-}
-
-resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = "${aws_lb_target_group.release_mgt_elb_target_group.arn}"
-  target_id        = "${aws_instance.test.id}"
-  port             = 80
-}
-
-##########################################################################################
 # INSTANCES
 ##########################################################################################
 data "aws_ami" "ubuntu" {
@@ -153,7 +131,7 @@ resource "aws_autoscaling_group" "release_mgt_asg" {
   desired_capacity = 1
   max_size = 1
   min_size = 1
-  vpc_zone_identifier = ["${aws_subnet.release_mgt_subnet.id}"]
+  vpc_zone_identifier = ["${aws_subnet.release_mgt_subnet_a.id}", "${aws_subnet.release_mgt_subnet_b.id}"]
   health_check_grace_period = 300
   health_check_type = "ELB"
   tag {
@@ -164,5 +142,69 @@ resource "aws_autoscaling_group" "release_mgt_asg" {
   launch_template {
     id = "${aws_launch_template.release_mgt_launch_template.id}"
     version = "$Latest"
+  }
+}
+
+##########################################################################################
+# LOAD BALANCER
+##########################################################################################
+resource "aws_lb_target_group" "release_mgt_elb_target_group" {
+  name = "releaseMgtTargetGroup"
+  port= 443
+  protocol = "HTTPS"
+  vpc_id = "${aws_vpc.release_mgt_vpc.id}"
+  target_type = "instance"
+  #TODO add health_check
+}
+
+resource "aws_lb" "release_mgt_elb" {
+  name = "releaseMgtElb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = ["${aws_security_group.release_mgt_sg.id}"]
+  subnets = ["${aws_subnet.release_mgt_subnet_a.id}", "${aws_subnet.release_mgt_subnet_b.id}"]
+  enable_deletion_protection = false
+  # access_logs {
+  #    bucket  = "${aws_s3_bucket.lb_logs.bucket}"
+  #    prefix  = "test-lb"
+  #    enabled = true
+  #  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.release_mgt_elb.arn}"
+  port = "443"
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = "arn:aws:acm:eu-west-3:496124100072:certificate/5d207839-3699-4bc7-b207-6f52ddd42fd3" #TODO variable or create or pre-condition ?
+  default_action {
+    type = "forward"
+    target_group_arn = "${aws_lb_target_group.release_mgt_elb_target_group.arn}"
+  }
+}
+
+#TODO add redirect from 80 to 443: https://www.terraform.io/docs/providers/aws/r/lb_listener.html
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = "${aws_autoscaling_group.release_mgt_asg.id}"
+  alb_target_group_arn   = "${aws_lb_target_group.release_mgt_elb_target_group.arn}"
+}
+
+##########################################################################################
+# ROUTE 53
+##########################################################################################
+data "aws_route53_zone" "selected" {
+  name = "releasemgt.net." #TODO parameter ?
+  private_zone = false
+}
+
+resource "aws_route53_record" "release_mgt_dns_record" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name = "greencity" #TODO parameter
+  type = "A"
+  alias {
+    name = "${aws_lb.release_mgt_elb.dns_name}"
+    zone_id = "${aws_lb.release_mgt_elb.zone_id}"
+    evaluate_target_health = false
   }
 }
