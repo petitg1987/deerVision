0699b289bcf33093dab8624276bc0920e8df707e
greg <petitg1987@gmail.com>
2021-10-19 11:14:31 +0200

    Update menu spacing

--- deer-vision-frontend/src/index.js
+++ deer-vision-frontend/src/index.js
@@ -28,30 +28,35 @@ class Index extends Component {
         return (
             <Router>
                 <div>
-                    <nav className="nav">
-                        <div className="nav-logo-and-hamburger">
-                            <Link to={'/'}>
-                                <img className="nav-logo" src={studioLogoMini} alt="Studio Logo" width="60" height="60" />
-                                <div className="nav-logo-text">Deer Vision Studio</div>
-                            </Link>
-                            <Link to="#" className="nav-hamburger-icon" onClick={() => this.navMenuIconClick()}>
-                                <svg viewBox="0 0 100 80" width="23" height="18.4">
-                                    <rect width="100" height="20" rx="8"/>
-                                    <rect y="30" width="100" height="20" rx="8"/>
-                                    <rect y="60" width="100" height="20" rx="8"/>
-                                </svg>
-                            </Link>
-                        </div>
-                        <ul className="nav-menu" id="nav-menu-id">
-                            <li className="nav-menu-link-container">
-                                <Link to={'/about-us'} className="nav-menu-link" onClick={() => this.navMenuLinkClick()}>About Us</Link>
-                            </li>
-                            <li className="nav-menu-link-container">
-                                <Link to={'/games'} className="nav-menu-link" onClick={() => this.navMenuLinkClick()}>Our Games</Link>
-                            </li>
-                        </ul>
-                        <div className="nav-right-spacing"/>
-                    </nav>
+                    <header>
+                        <nav className="nav">
+                            <div className="nav-logo-and-hamburger">
+                                <Link to={'/'}>
+                                    <img className="nav-logo" src={studioLogoMini} alt="Studio Logo" width="60"
+                                         height="60"/>
+                                    <div className="nav-logo-text">Deer Vision Studio</div>
+                                </Link>
+                                <Link to="#" className="nav-hamburger-icon" onClick={() => this.navMenuIconClick()}>
+                                    <svg viewBox="0 0 100 80" width="23" height="18.4">
+                                        <rect width="100" height="20" rx="8"/>
+                                        <rect y="30" width="100" height="20" rx="8"/>
+                                        <rect y="60" width="100" height="20" rx="8"/>
+                                    </svg>
+                                </Link>
+                            </div>
+                            <span className="nav-spacing">&nbsp;</span>
+                            <ul className="nav-menu" id="nav-menu-id">
+                                <li className="nav-menu-link-container">
+                                    <Link to={'/about-us'} className="nav-menu-link"
+                                          onClick={() => this.navMenuLinkClick()}>About Us</Link>
+                                </li>
+                                <li className="nav-menu-link-container">
+                                    <Link to={'/games'} className="nav-menu-link"
+                                          onClick={() => this.navMenuLinkClick()}>Our Games</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
                     <div className="content">
                         <Switch>
                             <Route exact path='/' component={Home}/>
