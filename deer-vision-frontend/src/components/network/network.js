315dba5f00557789f5285436a5d0f2f6d7839923
greg <petitg1987@gmail.com>
2021-10-20 15:31:57 +0200

    Add social network logo

---
+++ deer-vision-frontend/src/components/network/network.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import discordLogo from "../../images/discordLogo.png";
+import youtubeLogo from "../../images/youtubeLogo.png";
+import reeditLogo from "../../images/reeditLogo.png";
+import twitterLogo from "../../images/twitterLogo.png";
+import './network.css';
+
+class Network extends Component {
+
+    constructor(props) {
+        super(props);
+        this.logoSize = 50;
+
+    }
+
+    componentDidMount() {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const networkLogos = entry.target.querySelectorAll('.network-logo');
+                if (entry.isIntersecting) {
+                    networkLogos.forEach(e => e.classList.add('network-logo-animation'));
+                } else {
+                    networkLogos.forEach(e => e.classList.remove('network-logo-animation'));
+                }
+            });
+        });
+        observer.observe(document.querySelector('.network-links'));
+    }
+
+    render() {
+        return (
+            <div className="network-container">
+                <div className="network-title">Follow us on:</div>
+                <div className="network-links">
+                    <a href="https://discord.com/invite/XYZ1234" target="_blank" rel="noopener noreferrer">
+                        <img className="network-logo" src={discordLogo} alt="Discord Logo" width={this.logoSize} height={this.logoSize}/>
+                    </a>
+                    <a href="https://www.twitter.com/deervisionstudio" target="_blank" rel="noopener noreferrer">
+                        <img className="network-logo" src={twitterLogo} alt="Twitter Logo" width={this.logoSize} height={this.logoSize}/>
+                    </a>
+                    <a href="https://www.reddit.com/r/deervisionstudio" target="_blank" rel="noopener noreferrer">
+                        <img className="network-logo" src={reeditLogo} alt="Reedit Logo" width={this.logoSize} height={this.logoSize}/>
+                    </a>
+                    <a href="https://www.youtube.com/c/DeerVisionStudio" target="_blank" rel="noopener noreferrer">
+                        <img className="network-logo" src={youtubeLogo} alt="Youtube Logo" width={this.logoSize} height={this.logoSize}/>
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Network;
--- deer-vision-frontend/src/pages/main/about-us.js
+++ deer-vision-frontend/src/pages/main/about-us.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Network from "../../components/network/network";
 
 class AboutUs extends Component {
     render() {
         return (
             <div>
                 <h2>About Us</h2>
-                <br/>1<br/>2<br/>3<br/>4<br/>5<br/>6<br/>7<br/>8<br/>9<br/>0<br/>1<br/>2<br/>3<br/>4<br/>5<br/>6<br/>7<br/>8<br/>1<br/>2<br/>3<br/>4<br/>5<br/>6<br/>7<br/>8<br/>9
+                <Network/>
             </div>
         );
     }
