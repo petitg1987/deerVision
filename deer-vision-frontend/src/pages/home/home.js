58c5b52932dccc52f4f4a14a29336f4ef624699f
greg <petitg1987@gmail.com>
2021-10-21 15:22:04 +0200

    Improve social network for game

--- deer-vision-frontend/src/pages/home/home.js
+++ deer-vision-frontend/src/pages/home/home.js
@@ -67,8 +67,8 @@ class Home extends Component {
                     <div className="pres-description">
                         <Description>
                             <p>Deer Vision Studio is an <strong>independent</strong> studio, which develops video games. The studio is located in Belgium (Ardennes).</p>
-                            <p>Our philosophy is to publish high quality games and listen our community to constantly improve our games in the right direction. Therefore, it is always a real pleasure to discuss with you on our social networks (see links below).</p>
-                            <p>We like to create our games without any technical restrictions. This is why we have a home-made game engine named "Urchin Engine" that we have been developing for more than 10 years. The engine is free and open source: <a href="https://github.com/petitg1987/urchinEngine" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+                            <p>Our philosophy is to publish high quality games and to <strong>listen our community</strong> to constantly improve our games in the right direction. Therefore, it is always a real pleasure to discuss with you on our social networks (see links below).</p>
+                            <p>We like to create our games without any restrictions. This is why we have a home-made game engine (<a href="https://github.com/petitg1987/urchinEngine" target="_blank" rel="noopener noreferrer" title="GitHub">Urchin Engine</a>) and we produce most of our assets. This gives us a full control over the game development process and the only limitations are our imagination!</p>
                         </Description>
                     </div>
                     <div className="home-horizontal-spacer"/>
