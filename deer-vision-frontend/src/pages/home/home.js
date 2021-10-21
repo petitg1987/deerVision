29c6f69e0852d95e166b451b6736343757059f9d
greg <petitg1987@gmail.com>
2021-10-21 11:27:00 +0200

    Improve home page visual

--- deer-vision-frontend/src/pages/home/home.js
+++ deer-vision-frontend/src/pages/home/home.js
@@ -23,19 +23,22 @@ class Home extends Component {
                 <div id="games"/>
                 <h2>Our Games</h2>
                 <div className="pe-container">
-                    <div className="home-separator"/>
+                    <div className="home-horizontal-spacer"/>
                     <div className="pe-title">
                         <img src={photonEngineerLogo} alt="Twitter Logo" width="300px" height="75px"/>
                     </div>
-                    <div className="home-separator"/>
+                    <div className="home-horizontal-spacer"/>
                     <div className="pe-description">
                         <Description>
                             <p>Photon Engineer is a <strong>puzzle/building game</strong>. Your goal is to build an automated, efficient and optimized system which fit with the imposed constraints.</p>
-                            <p>Community links: Discord, Twitter, Reedit</p>
+                            <p>Community links: TODO</p>
+                            <p>Steam key request: Keymailer</p>
+                            <p>Steam key request: Keymailer</p>
+                            <p>Steam key request: Keymailer</p>
                             <p>Steam key request: Keymailer</p>
                         </Description>
                     </div>
-                    <div className="home-separator"/>
+                    <div className="home-horizontal-spacer"/>
                     <div className="pe-btn-platform">
                         <Button text="WATCH TRAILER" link="https://www.youtube.com/watch?v=XYZ1234"/>
                         <br/>
@@ -45,7 +48,7 @@ class Home extends Component {
                     </div>
                     <br/>
                     <div className="pe-screenshots">
-                        <Carousel autoPlay={true} interval={3000} showThumbs={false} infiniteLoop={true} showStatus={false}>
+                        <Carousel autoPlay={false} showThumbs={false} infiniteLoop={false} showStatus={false}>
                             <div>
                                 <img src="/photon-engineer/screenshot1.png" alt="Photon Engineer screenshot 1" />
                             </div>
@@ -64,7 +67,7 @@ class Home extends Component {
                 <div id="aboutUs"/>
                 <h2>About Us</h2>
                 <div className="pres-container">
-                    <div className="home-separator"/>
+                    <div className="home-horizontal-spacer"/>
                     <div className="pres-description">
                         <Description>
                             <p>Deer Vision Studio is an <strong>independent</strong> studio, which develops video games. The studio is located in Belgium (Ardennes).</p>
@@ -72,7 +75,7 @@ class Home extends Component {
                             <p>We like to create our games without any technical restrictions. This is why we have a home-made game engine named "Urchin Engine" that we have been developing for more than 10 years. The engine is free and open source: <a href="https://github.com/petitg1987/urchinEngine" target="_blank" rel="noopener noreferrer">GitHub</a></p>
                         </Description>
                     </div>
-                    <div className="home-separator"/>
+                    <div className="home-horizontal-spacer"/>
                     <div className="pres-logo">
                         <img src={studioLogo} alt="Studio Logo" width="300" height="300"/>
                     </div>
