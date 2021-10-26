7ee9b8626d56693791a869d7b8eb31c627d0859d
petitg1987 <petitg1987@gmail.com>
2021-10-27 00:31:48 +0200

    Menu link: prevent default

--- deer-vision-frontend/src/components/navigation/navigation.js
+++ deer-vision-frontend/src/components/navigation/navigation.js
@@ -5,7 +5,8 @@ import './navigation.css';
 
 class Navigation extends Component {
 
-    navMenuIconClick() {
+    navMenuIconClick(evt) {
+        evt.preventDefault();
         let menu = document.getElementById("nav-menu-id");
         if (menu.classList.contains("responsive")) {
             menu.classList.remove("responsive");
@@ -23,17 +24,17 @@ class Navigation extends Component {
         return (
             <nav className="nav">
                 <div className="nav-logo-and-hamburger">
-                    <Link to="/#">
+                    <a href="/#">
                         <img className="nav-logo" src={studioLogoMini} alt="Studio Logo" width="61" height="61"/>
                         <div className="nav-logo-text">Deer Vision Studio</div>
-                    </Link>
-                    <Link to="#" className="nav-hamburger-icon" onClick={() => this.navMenuIconClick()}>
+                    </a>
+                    <a href="/" className="nav-hamburger-icon" onClick={(evt) => this.navMenuIconClick(evt)}>
                         <svg viewBox="0 0 100 100" width="23" height="21">
                             <rect fill="#ffffff" y="0"  width="100" height="21" rx="8"/>
                             <rect fill="#ffffff" y="39" width="100" height="21" rx="8"/>
                             <rect fill="#ffffff" y="79" width="100" height="21" rx="8"/>
                         </svg>
-                    </Link>
+                    </a>
                 </div>
                 <div className="nav-spacing">&nbsp;</div>
                 <ul className="nav-menu-ul" id="nav-menu-id">
