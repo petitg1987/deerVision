5baec3918588da147d8b50d37980c6b60ce470be
petitg1987 <petitg1987@gmail.com>
2022-05-01 17:49:55 +0200

    Add photon engineer level controller

---
+++ deer-vision-backend/src/main/java/studio/deervision/controller/pe/LevelRestController.java
@@ -0,0 +1,36 @@
+package studio.deervision.controller.pe;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import studio.deervision.service.pe.LevelService;
+
+@RestController
+@RequestMapping("/api")
+public class LevelRestController {
+
+    private final LevelService levelService;
+
+    @Autowired
+    public LevelRestController(LevelService levelService) {
+        this.levelService = levelService;
+    }
+
+    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 0-17" --data "3" "http://localhost:5000/api/pe/level/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
+    @PostMapping(value = "/pe/level/{id}/completionTime", consumes = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> levelCompletionTime(@PathVariable("id") Long levelId, @RequestBody String value, @RequestParam("appId") String appId, @RequestParam(value="appVersion") String appVersion) {
+        String requestKey = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        long completionTimeInSec;
+        try {
+            completionTimeInSec = Long.parseLong(value);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        levelService.registerLevelCompletionTime(requestKey, appId, appVersion, levelId, completionTimeInSec);
+        return ResponseEntity.ok(null);
+    }
+
+}
---
+++ deer-vision-backend/src/main/java/studio/deervision/service/pe/LevelService.java
@@ -0,0 +1,12 @@
+package studio.deervision.service.pe;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LevelService {
+
+    public void registerLevelCompletionTime(String requestKey, String appId, String appVersion, long levelId, long completionTimeInSec) {
+        //TODO impl...
+    }
+
+}
