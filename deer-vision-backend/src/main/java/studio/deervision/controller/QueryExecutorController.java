d20f18fd299a4b4da3db451467efd4bf6e57552d
petitg1987 <petitg1987@gmail.com>
2022-05-03 11:02:04 +0200

    Add query executor

--- deer-vision-backend/src/main/java/studio/deervision/controller/QueryExecutorController.java
+++ deer-vision-backend/src/main/java/studio/deervision/controller/QueryExecutorController.java
@@ -20,8 +20,8 @@ public class QueryExecutorController {
         this.queryExecutorService = queryExecutorService;
     }
 
-    //curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkdnNKV1QiLCJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzQ1NzIzODgsImV4cCI6MTk0OTkzMjM4OH0.S-VnMofcbTMv4epZCT3Es1zezcvXsN4xL0gmkXca3vGHsXvwa5MB1puaw6Y8wBUZLLifvXLLGZUcYvYoDvLOWQ" -H "Content-Type: text/plain" --data "select * from issue" "http://localhost:5000/api/admin/query-select"
-    //curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkdnNKV1QiLCJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzQ1NzIzODgsImV4cCI6MTk0OTkzMjM4OH0.S-VnMofcbTMv4epZCT3Es1zezcvXsN4xL0gmkXca3vGHsXvwa5MB1puaw6Y8wBUZLLifvXLLGZUcYvYoDvLOWQ" -H "Content-Type: text/plain" --data "select * from issue" "https://backend.deervision.studio/api/admin/query-select"
+    //curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkdnNKV1QiLCJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzQ1NzIzODgsImV4cCI6MTk0OTkzMjM4OH0.S-VnMofcbTMv4epZCT3Es1zezcvXsN4xL0gmkXca3vGHsXvwa5MB1puaw6Y8wBUZLLifvXLLGZUcYvYoDvLOWQ" -H "Content-Type: text/plain" --data "select * from issue where value like '%GTX 980 Ti%'" "http://localhost:5000/api/admin/query-select"
+    //curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkdnNKV1QiLCJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzQ1NzIzODgsImV4cCI6MTk0OTkzMjM4OH0.S-VnMofcbTMv4epZCT3Es1zezcvXsN4xL0gmkXca3vGHsXvwa5MB1puaw6Y8wBUZLLifvXLLGZUcYvYoDvLOWQ" -H "Content-Type: text/plain" --data "select * from issue where value like '%GTX 980 Ti%'" "https://backend.deervision.studio/api/admin/query-select"
     @PostMapping(value = "/admin/query-select")
     public ResponseEntity<String> querySelect(@RequestBody String sqlQuery) {
         try {
@@ -34,7 +34,7 @@ public class QueryExecutorController {
         }
     }
 
-    //curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkdnNKV1QiLCJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzQ1NzIzODgsImV4cCI6MTk0OTkzMjM4OH0.S-VnMofcbTMv4epZCT3Es1zezcvXsN4xL0gmkXca3vGHsXvwa5MB1puaw6Y8wBUZLLifvXLLGZUcYvYoDvLOWQ" -H "Content-Type: text/plain" --data "create index index_levelId on level_completion_time (level_id)" "https://backend.deervision.studio/api/admin/query-update"
+    //curl -X POST -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkdnNKV1QiLCJzdWIiOiJhZG1pbiIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzQ1NzIzODgsImV4cCI6MTk0OTkzMjM4OH0.S-VnMofcbTMv4epZCT3Es1zezcvXsN4xL0gmkXca3vGHsXvwa5MB1puaw6Y8wBUZLLifvXLLGZUcYvYoDvLOWQ" -H "Content-Type: text/plain" --data "delete from issue where value like '%GeForce RTX 2080 SUPER%'" "https://backend.deervision.studio/api/admin/query-update"
     @PostMapping(value = "/admin/query-update")
     public ResponseEntity<String> queryUpdate(@RequestBody String sqlQuery) {
         try {
