353948fefaadacdf3b5e0df85dd1520a665ddfdc
petitg1987 <petitg1987@gmail.com>
2022-06-24 15:41:13 +0200

    Use flexbox for about page

--- deer-vision-frontend/src/pages/about/about.js
+++ deer-vision-frontend/src/pages/about/about.js
@@ -11,17 +11,14 @@ class About extends Component {
             <div>
                 <h2>About</h2>
                 <div className="pres-container">
-                    <div className="vertical-spacer"/>
-                    <div className="horizontal-spacer"/>
                     <div className="pres-description">
+                        <p><img src={gregProfile} alt="Greg profile" width="150" height="186" className="pres-profile"/></p>
                         <Description>
-                            <p><img src={gregProfile} alt="Greg profile" width="150" height="186" className="pres-profile"/></p>
                             <p>My name is Grégory Petit and I'm a <strong>solo game developer</strong> located in Belgium. Deer Vision Studio is the company name used to publish and sell the games.</p>
                             <p>My philosophy is to publish high quality games and <strong>listen the community</strong> to constantly improve them in the right direction.</p>
                             <p>I like to create my games without any restrictions. That is why I developed my own game engine (<a className="text-link" href="https://github.com/petitg1987/urchinEngine" target="_blank" rel="noopener noreferrer" title="Git Hub">Urchin Engine</a>) and I produce most of the game assets. This gives me a full control over the game development process and the only limit is my imagination!</p>
                         </Description>
                     </div>
-                    <div className="horizontal-spacer"/>
                     <div className="pres-logo">
                         <img src={studioLogo} alt="Studio Logo" width="300" height="300"/>
                     </div>
