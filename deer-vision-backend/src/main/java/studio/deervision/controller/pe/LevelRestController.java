afff0e2747f87b2dd0c21d59cfaaa1ebaacfd269
petitg1987 <petitg1987@gmail.com>
2022-07-10 13:54:32 +0200

    Adapt backend to accept several completion time

--- deer-vision-backend/src/main/java/studio/deervision/controller/pe/LevelRestController.java
+++ deer-vision-backend/src/main/java/studio/deervision/controller/pe/LevelRestController.java
@@ -24,10 +24,10 @@ public class LevelRestController {
         this.levelService = levelService;
     }
 
-    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 0-17" --data "cageDoorOpened:122" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
-    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 1-18" --data "cageDoorOpened:181" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
-    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 2-19" --data "cageDoorOpened:185" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
-    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 0-17" --data "puzzleCompleted:182" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
+    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 0-17" --data "Open Cage Door:122" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
+    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 1-18" --data "Open Cage Door:181" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
+    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 2-19" --data "Open Cage Door:185" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
+    //curl -X POST -H "Content-Type: text/plain" -H "X-Key: 0-17" --data "Complete Puzzle:182" "http://localhost:5000/api/pe/levels/0/completionTime?appId=photonEngineer&appVersion=1.0.0"
     @PostMapping(value = "/pe/levels/{levelId}/completionTime", consumes = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> levelCompletionTime(@PathVariable("levelId") Integer levelId, @RequestBody String value, @RequestParam("appId") String appId, @RequestParam(value="appVersion") String appVersion) {
         String requestKey = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
