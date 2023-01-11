348bfbf6249a765a64d637a33325cd22ce8885b2
petitg1987 <petitg1987@gmail.com>
2023-01-11 08:36:24 +0100

    Update max completion time max to 60 min

--- deer-vision-backend/src/main/java/studio/deervision/service/ActionCompletionTimeService.java
+++ deer-vision-backend/src/main/java/studio/deervision/service/ActionCompletionTimeService.java
@@ -1,6 +1,5 @@
 package studio.deervision.service;
 
-import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
