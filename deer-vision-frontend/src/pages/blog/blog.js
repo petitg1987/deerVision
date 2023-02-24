5f1cc38f0a36eabcbf741fbe9d4285f47cd63385
petitg1987 <petitg1987@gmail.com>
2023-02-24 18:02:49 +0100

    Add blog section

--- deer-vision-frontend/src/pages/blog/blog.js
+++ deer-vision-frontend/src/pages/blog/blog.js
@@ -12,7 +12,7 @@ class Blog extends Component {
                     <div className="blog-preview">
                         <div className={"blog-title"}><a className={"text-link"} href={"/blog/consumption"}>I measured the electricity consumption of my game</a></div>
                         <div className={"blog-preview-main"}>
-                            <img className={"blog-img"} src={blogConsumptionImg} width={150} height={150} alt={"Device to measure electricity consumption"}/>
+                            <img className={"blog-img"} src={blogConsumptionImg} width={130} height={130} alt={"Device to measure electricity consumption"}/>
                             <div>As a programmer, I've always wondered how much difference an optimized application can make to the electricity consumed by a computer. What's better than testing on my own puzzle game?</div>
                         </div>
                         <div className={"blog-metadata"}>date: 2023-02-24, poster: Grégory Petit</div>
--- deer-vision-frontend/src/pages/blog/consumption.js
+++ deer-vision-frontend/src/pages/blog/consumption.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "../pages.css"
 import "./blog.css"
+import blogConsumptionStartImg from "../../images/blogConsumptionStart.webp"
 
 class Consumption extends Component {
     render() {
@@ -8,8 +9,34 @@ class Consumption extends Component {
             <div>
                 <h2>Electricity consumption of my game</h2>
                 <div className="blog-container">
-                    <div className="blog-preview">
-                        blabla
+                    <div className="blog-content">
+                        <p>Have you ever wondered how much electricity a video game consumes? This is the kind of question I ask myself out of curiosity.</p>
+                        <img className={"blog-img-trans"} src={blogConsumptionStartImg} width={341} height={512} alt={"An electric pole"}/>
+
+                        <div className={"blog-sub-title"}>Setup</div>
+                        <p>My desktop computer:
+                            <ul>
+                                <li>CPU: Intel i7-8700K CPU @ 3.70GHz</li>
+                                <li>Graphics card: NVidia 2080 Super</li>
+                                <li>RAM: 32Go</li>
+                                <li>Disk: 980 PRO PCle 4.0 NVMe M.2 SSD</li>
+                                <li>Two screens - 4K</li>
+                            </ul>
+                        </p>
+
+                        <p>The game (<a className={"text-link"} href={"https://store.steampowered.com/app/2305110/Photon_Engineer/"} target={"_blank"} rel="noreferrer">Photon Engineer</a>):
+                            <ul>
+                                <li>3D environment but with limited number of models</li>
+                                <li>Bottleneck on the GPU (CPU usage: ~20%)</li>
+                            </ul>
+                        </p>
+
+                        <p>Measurement device:
+                            <ul>
+                                <li>I simply plug my computer on a device measuring the electricity consumption</li>
+                            </ul>
+                        </p>
+
                     </div>
                 </div>
             </div>
