dc857f5ba07fbcdf9f0e4ce7d8fc4eaa5cee6d13
petitg1987 <petitg1987@gmail.com>
2023-03-14 13:24:19 +0100

    Add press kit page

--- deer-vision-frontend/src/pages/games/pe-presskit.js
+++ deer-vision-frontend/src/pages/games/pe-presskit.js
@@ -10,7 +10,7 @@ class PePressKit extends Component {
                 <div className={"press-container"}>
                     <div className={"press-content"}>
                         <div>If you are a YouTuber, Twitch streamer, influencer or journalist, feel free to use our press kit for any content related to Photon Engineer.</div>
-                        <div>To learn more about the game, please consult our <a className={"text-link"} href={"/"}>games</a> page.</div>
+                        <div>To learn more about the Photon Engineer, please consult our <a className={"text-link"} href={"/"}>games</a> page.</div>
                         <div className={"press-title"}>Global</div>
                         <ul>
                             <li><strong>Name</strong>: Photon Engineer</li>
@@ -18,7 +18,7 @@ class PePressKit extends Component {
                             <li><strong>Steam page</strong>: <a className={"text-link"} target="_blank" rel="noopener noreferrer" href={"https://store.steampowered.com/app/2305110/Photon_Engineer/"}>https://store.steampowered.com/app/2305110/Photon_Engineer/</a></li>
                             <li><strong>Creator / publisher</strong>: Grégory Petit</li>
                             <li><strong>Game Engine</strong>: <a className={"text-link"} target="_blank" rel="noopener noreferrer" href={"https://github.com/petitg1987/urchinEngine"}>Urchin Engine</a> (in-house engine)</li>
-                            <li><strong>Development time</strong>: ~2 years (full time) on the game & ~10 years (spare time) on the game engine</li>
+                            <li><strong>Development time</strong>: ~2 years (full time) on the game, ~10 years (spare time) on the game engine</li>
                         </ul>
 
                         <div className={"press-title"}>Assets</div>
