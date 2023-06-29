bed80ab988ea1b2b996ce413c0ae9550629a5128
petitg1987 <petitg1987@gmail.com>
2023-06-29 16:38:19 +0200

    Update mount disk

--- setup/infrastructure/instancesSetupScript.tmpl.sh
+++ setup/infrastructure/instancesSetupScript.tmpl.sh
@@ -1,18 +1,34 @@
 #!/bin/bash -x
 
+#Create script log file
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Start script execution" >> /home/ubuntu/userdata.log
+
 #Mount (and format) volume
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Mounting data volume" >> /home/ubuntu/userdata.log
+sleep 5 #wait to be sure volume is available
 cd /home/ubuntu/ || exit
 sudo mkdir ./data
 volumeName="/dev/"$(lsblk | grep 'disk' | grep '4G' | cut -f 1 -d ' ')
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Volume name is $volumeName" >> /home/ubuntu/userdata.log
 checkVolume=$(sudo file -s $volumeName)
 if [[ "$checkVolume" == "$volumeName: data" ]]; then
-  #Volume is empty: format to ext4
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Formatting empty data volume in ext4" >> /home/ubuntu/userdata.log
   sudo mkfs -t ext4 $volumeName
 fi
-sudo mount $volumeName /home/ubuntu/data
+echo "$volumeName   /home/ubuntu/data   ext4   defaults   0   0" | sudo tee -a /etc/fstab
+sudo mount -a
+sleep 5 #wait to be sure volume is mounted
 sudo chown ubuntu:ubuntu -R ./data
+checkVolumeMountedResult=$(df -h)
+if [[ $checkVolumeMountedResult == *"/home/ubuntu/data"* ]]; then
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Volume is correctly mounted" >> /home/ubuntu/userdata.log
+else
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Volume mount fail" >> /home/ubuntu/userdata.log
+  exit 1
+fi
 
 #Launch database
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Launching database" >> /home/ubuntu/userdata.log
 mkdir -p /home/ubuntu/data/db
 sudo docker run --restart always --name deervision-db -e POSTGRES_PASSWORD=vY82M3ZrnZEN -v /home/ubuntu/data/db/:/var/lib/postgresql/data -p 5432:5432 -d postgres:14.5
 sleep 10 #Wait volume is created by docker
@@ -23,14 +39,19 @@ mkdir -p /home/ubuntu/data/letsencrypt/logs
 mkdir -p /home/ubuntu/data/letsencrypt/work
 mkdir -p /home/ubuntu/data/letsencrypt/config
 if [ ! -d "/home/ubuntu/data/letsencrypt/config/live/backend.deervision.studio" ]; then
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Creating certificate" >> /home/ubuntu/userdata.log
   sudo certbot -n --agree-tos -m petitg1987@gmail.com --logs-dir /home/ubuntu/data/letsencrypt/logs --work-dir /home/ubuntu/data/letsencrypt/work --config-dir /home/ubuntu/data/letsencrypt/config certonly --dns-route53 -d backend.deervision.studio
+else
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Certificate already exist" >> /home/ubuntu/userdata.log
 fi
 
 #Schedule renew certificate (Let's encrypt)
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Scheduling renew certificate" >> /home/ubuntu/userdata.log
 echo -e "#"'!'"/bin/bash\n\ncertbot --logs-dir /home/ubuntu/data/letsencrypt/logs --work-dir /home/ubuntu/data/letsencrypt/work --config-dir /home/ubuntu/data/letsencrypt/config renew --dns-route53\nsudo service nginx reload" | sudo tee /etc/cron.daily/deervision-renew-cert
 sudo chmod +x /etc/cron.daily/deervision-renew-cert
 
 #Setup Nginx
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Installing nginx" >> /home/ubuntu/userdata.log
 echo -e "limit_req_zone \$binary_remote_addr zone=req_zone:500m rate=${maxRequestsBySecond}r/s;\n" \
   "server {\n" \
   "  listen 443;\n" \
@@ -54,6 +75,7 @@ sudo ln -s /etc/nginx/sites-available/reverseproxy /etc/nginx/sites-enabled/reve
 sudo systemctl restart nginx
 
 #Setup CodeDeploy agent (instructions for Ubuntu 22.04 from: https://github.com/aws/aws-codedeploy-agent/issues/301)
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Installing code deploy" >> /home/ubuntu/userdata.log
 cd /tmp
 wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
 mkdir codedeploy-agent_1.3.2-1902_ubuntu22
@@ -67,6 +89,7 @@ cd /home/ubuntu/
 #Setup CloudWatch agent
 # - Note 1: JSON configuration logs file is /opt/aws/amazon-cloudwatch-agent/logs/configuration-validation.log
 # - Note 2: execution logs file is /opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Installing cloud watch agent" >> /home/ubuntu/userdata.log
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 cat <<EOT > ./amazon-cloudwatch-agent.json
@@ -119,3 +142,7 @@ cat <<EOT > ./amazon-cloudwatch-agent.json
 EOT
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/amazon-cloudwatch-agent.json -s
 sudo rm amazon-cloudwatch-agent.deb
+
+#End script log file
+echo "$(date '+%Y-%m-%d %H:%M:%S') - End script execution" >> /home/ubuntu/userdata.log
+echo "" >> /home/ubuntu/userdata.log
