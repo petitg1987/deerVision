a37d7cbfa448f755264d6af63f37054eb1a8fa3a
petitg1987 <petitg1987@gmail.com>
2023-11-23 10:05:17 +0100

    Use github workflow

--- deer-vision-backend/deploy/deploy-backend.sh
+++ deer-vision-backend/deploy/deploy-backend.sh
@@ -15,6 +15,7 @@ echo "Log into AWS ECR to get the last image id"
 aws ecr get-login-password --region $AWS_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 image_info=$(aws ecr describe-images --region $AWS_REGION --repository-name $DOCKER_REGISTRY_NAME --query 'imageDetails[].[imageTags[0], imagePushedAt]' --output text)
 latest_tag=$(echo "$image_info" | sort -k2 -r | awk '{print $1}' | head -n 1)
+current_tag=$(sudo docker ps --filter "name=deervision" | grep -E " deervision$" | awk '{print $2}')
 
 echo "Deploy the Docker image"
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$DOCKER_REGISTRY_NAME:$latest_tag
@@ -29,5 +30,7 @@ sudo docker image prune -a -f
 sudo docker system prune -a -f
 image_ids=$(aws ecr list-images --region $AWS_REGION --repository-name $DOCKER_REGISTRY_NAME --query 'imageIds[].imageDigest' --output text)
 for image_id in $image_ids; do
-  aws ecr batch-delete-image --region $AWS_REGION --repository-name $DOCKER_REGISTRY_NAME --image-ids imageDigest=$image_id
+  if [ "$image_id" != "$latest_tag" ] && [ "$image_id" != "$current_tag" ]; then
+    aws ecr batch-delete-image --region $AWS_REGION --repository-name $DOCKER_REGISTRY_NAME --image-ids imageDigest=$image_id
+  fi
 done
