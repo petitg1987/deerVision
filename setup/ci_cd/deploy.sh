fd28ea72c6653d3aefff3036dd2ade8a7a0588da
petitg1987 <petitg1987@gmail.com>
2025-09-23 21:15:55 +0200

    Update deploy

--- setup/ci_cd/deploy.sh
+++ setup/ci_cd/deploy.sh
@@ -8,6 +8,10 @@ cd "$(dirname "$0")"
 echo "Starting to deploy the backend image"
 docker stop deer-vision-backend || true
 docker rm deer-vision-backend || true
+
+sudo docker network create "app-dv-network" || true
+sudo docker network connect "app-dv-network" "deer-vision-db" || true
+
 docker run -d \
     -p 13002:8080 \
     -e DB_PASSWORD="$DB_PASSWORD" \
@@ -15,4 +19,5 @@ docker run -d \
     -e ADMIN_JWT_SECRET="$ADMIN_JWT_SECRET" \
     --restart always \
     --name deer-vision-backend \
+    --network="app-dv-network"
     deer-vision-backend:latest
