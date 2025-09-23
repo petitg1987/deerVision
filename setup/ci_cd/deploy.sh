29254a3f0496bf37a8859b76c7f40ba8290c4b6d
petitg1987 <petitg1987@gmail.com>
2025-09-23 22:40:44 +0200

    Update deploy

--- setup/ci_cd/deploy.sh
+++ setup/ci_cd/deploy.sh
@@ -3,8 +3,6 @@
 set -e
 cd "$(dirname "$0")"
 
-#TODO provide password from ansible variable (use file / volume ?)
-
 echo "Starting to deploy the backend image"
 docker stop deer-vision-backend || true
 docker rm deer-vision-backend || true
@@ -12,11 +10,15 @@ docker rm deer-vision-backend || true
 docker network create "deer-vision-network" || true
 docker network connect "deer-vision-network" "deer-vision-db" || true
 
+dbPassword=$(cat /data/secret/deerVisionDbPassword)
+adminPassword=$(cat /data/secret/deerVisionAdminPassword)
+adminJwtToken=$(cat /data/secret/deerVisionAdminJwtSecret)
+
 docker run -d \
     -p 13002:8080 \
-    -e DB_PASSWORD="$DEER_VISION_DB_PASSWORD" \
-    -e ADMIN_PASSWORD="$DEER_VISION_ADMIN_PASSWORD" \
-    -e ADMIN_JWT_SECRET="$DEER_VISION_ADMIN_JWT_SECRET" \
+    -e DB_PASSWORD="$dbPassword" \
+    -e ADMIN_PASSWORD="$adminPassword" \
+    -e ADMIN_JWT_SECRET="$adminJwtToken" \
     --restart always \
     --name deer-vision-backend \
     --network="deer-vision-network" \
