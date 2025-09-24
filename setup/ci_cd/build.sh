5bda1ea9b36874e076c8c99ff59f725bbd5822a9
petitg1987 <petitg1987@gmail.com>
2025-09-24 19:42:10 +0200

    Update deploy

--- setup/ci_cd/build.sh
+++ setup/ci_cd/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e
+cd "$(dirname "$0")"
 
 echo "Starting to build the backend image"
-cd "$(dirname "$0")"
 cd ../../deer-vision-backend/
 mvn -B clean package
-mkdir -p /img_backend
-rm -rf /img_backend/*
-rm -rf /img_backend/.*
-cp ./target/deer-vision*.jar /img_backend/deer-vision-backend.jar
-cp ../setup/ci_cd/backend/Dockerfile /img_backend/Dockerfile
-docker build -t deer-vision-backend:latest /img_backend
+mkdir -p /tmp/img_backend
+rm -rf /tmp/img_backend/*
+rm -rf /tmp/img_backend/.*
+cp ./target/deer-vision*.jar /tmp/img_backend/deer-vision-backend.jar
+cp ../setup/ci_cd/backend/Dockerfile /tmp/img_backend/Dockerfile
+docker build -t deer-vision-backend:latest /tmp/img_backend
