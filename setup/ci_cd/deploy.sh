cf6423c35faba4d44a930115b451270fdb3e2974
petitg1987 <petitg1987@gmail.com>
2025-09-25 19:28:26 +0200

    Update deploy

--- setup/ci_cd/deploy.sh
+++ setup/ci_cd/deploy.sh
@@ -4,12 +4,12 @@ set -e
 cd "$(dirname "$0")"
 directory_dir=$1
 
-echo "Starting the deployment for repository: $directory_dir"
+echo "Starting the deployment for repository: ${directory_dir}"
 
-cd "$directory_dir"/setup/ci_cd/backend/
+cd "${directory_dir}/setup/ci_cd/backend/"
 chmod +x ./deploy.sh
 ./deploy.sh "$directory_dir"
 
-cd "$directory_dir"/setup/ci_cd/frontend/
+cd "${directory_dir}/setup/ci_cd/frontend/"
 chmod +x ./deploy.sh
 ./deploy.sh "$directory_dir"
